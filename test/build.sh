e222d08e63d23d2e4bd7501196747dda2f61f415
Nikos Plugachev <nikos@plugachev.com>
2022-10-19 11:55:25 -0400

    Fixed gitignore

--- test/build.sh
+++
@@ -1,5 +0,0 @@
-
-cionom-cli.out --emit-bytecode=main.ibc main.cio
-cionom-cli.out --emit-bytecode=test.ibc test.cio
-cionom-cli.out --bundle=exe.cbe main.ibc test.ibc
-cionom-cli.out --execute-bundle exe.cbe 
